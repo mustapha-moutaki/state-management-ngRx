9e688432de0255a38568cc7777c6516d11f5a10b
mustapha <mustaphaamoutaki@gmail.com>
2026-01-24 18:50:30 +0100

    feat: add selectors and it's simple

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,21 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideStore } from '@ngrx/store';
 
 import { routes } from './app.routes';
+import { counterReducer } from './state/counter.reducer';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+
+    /**
+     * Register NgRx Store globally
+     * 'counter' is the state key
+     */
+    provideStore({
+      counter: counterReducer
+    })
   ]
 };
