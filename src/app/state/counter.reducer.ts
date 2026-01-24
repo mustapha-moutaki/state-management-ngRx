5dda32a7dcd9327be3c9afb8c3cd61d3a70aa842
mustapha <mustaphaamoutaki@gmail.com>
2026-01-24 18:47:33 +0100

    feat: fix errors

--- src/app/state/counter.reducer.ts
+++ src/app/state/counter.reducer.ts
@@ -14,3 +14,21 @@ export const initialState = 0;
 export const counterReducer = createReducer(
   initialState,
 
+  /**
+   * When increment action is dispatched
+   * We return a NEW state with +1
+   */
+  on(CounterActions.increment, (state) => state + 1),
+
+  /**
+   * When decrement action is dispatched
+   * We return a NEW state with -1
+   */
+  on(CounterActions.decrement, (state) => state - 1),
+
+  /**
+   * When reset action is dispatched
+   * We ignore the current state and return 0
+   */
+  on(CounterActions.reset, () => initialState)
+);
