41030820d59c926da7905f37c1756c85d0da7672
mustapha <mustaphaamoutaki@gmail.com>
2026-01-24 18:46:16 +0100

    feat: add customize comments for understand

--- src/app/state/counter.action.ts
+++ src/app/state/counter.action.ts
@@ -1,8 +1,25 @@
+import { createAction } from '@ngrx/store';
 
+/**
+ * This action is responsible for incrementing the counter value
+ * Basically telling the store: add +1
+ */
+export const increment = createAction(
+  '[Counter] Increment'
+);
 
+/**
+ * This action is responsible for decrementing the counter value
+ * Telling the store: subtract -1
+ */
+export const decrement = createAction(
+  '[Counter] Decrement'
+);
 
-
-
-
-
-
+/**
+ * This action resets the counter back to zero
+ * Useful for reset, logout, or reinitializing state
+ */
+export const reset = createAction(
+  '[Counter] Reset'
+);
